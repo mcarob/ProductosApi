30d3a653a11f24ffb848d2700f30d1a048a106a9
mcarob <mcarob@unbosque.edu.co>
2020-12-15 21:54:36 -0500

    usuario api

---
+++ db/usuario_db.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import Dict
+
+class usuarioInDB(BaseModel):
+    cod_usuario: int = 0
+    correo_usuario: str
+    nom_usuario: str
+    pass_usuario: str
+
+database_usuario = Dict[int, usuarioInDB]
+database_usuario={
+            1: usuarioInDB(**{  "cod_usuario":1,
+                                "correo_usuario":"Ariana@gmail.com",
+                                "nom_usuario":"Ariana",
+                                "pass_usuario":"81dc9bdb52d04dc20036dbd8313ed055"
+                            }),
+            2: usuarioInDB(**{  "cod_usuario":2,
+                                "correo_usuario":"Camilo@gmail.com",
+                                "nom_usuario":"Camilo Gutierrez",
+                                "pass_usuario":"81dc9bdb52d04dc20036dbd8313ed055"
+                            }),
+            3: usuarioInDB(**{  "cod_usuario":3,
+                                "correo_usuario":"carolina@gmail.com",
+                                "nom_usuario":"carolina",
+                                "pass_usuario":"81dc9bdb52d04dc20036dbd8313ed055"
+                            }),
+            4: usuarioInDB(**{  "cod_usuario":4,
+                                "correo_usuario":"felipe@gmail.com",
+                                "nom_usuario":"felipe",
+                                "pass_usuario":"81dc9bdb52d04dc20036dbd8313ed055"
+                            }),
+                    }
+generator = {"id":3}
+
+def save_category(usuario : usuarioInDB):
+    # se genera el numero para ingresar a la base de datos (en mysql auto_increment en postgres serial)
+    generator["id"]=generator["id"]+1
+    usuario.cod_usuario=generator["id"]
+    database_usuario[generator["id"]]=usuario
+    return usuario
+
+def get_usuario(username : str):
+    for i in database_usuario:
+        if database_usuario[i].correo_usuario == username:
+            return database_usuario[i]
+    return None
+
--- main.py
+++ main.py
@@ -2,16 +2,29 @@ from db.category_db import CategoryInDB,get_category,save_category
 from db.inventory_db import *
 from db.product_db import *
 from db.sale_db import *
-
+from db.usuario_db import *
+from models.usuario import *
 from models.inventory import *
 from models.product import *
 from models.category import categoryIn,categoryOut
-
 import datetime
+import hashlib 
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 products = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+products.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @products.get("/categorias/{peticion}")
 async def getCategorias(peticion: str):
@@ -30,3 +43,15 @@ async def getCategorias(peticion: str):
 async def ingresar_categoria(registro_categoria: CategoryInDB):
     nuevaCategoria=CategoryInDB(**registro_categoria.dict())
     return save_category(nuevaCategoria)
+
+
+@products.post("/usuarios/auth/")
+async def buscarUsuario(usuarioC : usuarioIn):
+    respuestadb=get_usuario(usuarioC.correo_usuario)
+    if respuestadb!= None :
+        if(respuestadb.pass_usuario==usuarioC.pass_usuario):
+            return usuarioOut(**respuestadb.dict())
+        else:
+            raise HTTPException(status_code=201, detail="La contraseña es incorrecta")
+    else:
+        raise HTTPException(status_code=201, detail="El correo no existe en la base de datos")
